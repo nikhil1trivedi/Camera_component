940d418ab4bbefc6f8770d2d6dff33a088715888
Hetesh Sehgal <heteshsehgal@gmail.com>
2018-04-06 11:29:15 -0700

    Update Camera_Main
    Include method for detection of camera, and to return camera object.

--- app/src/main/java/com/example/nikhiltrivedi/camera_component/CameraMain.java
+++ app/src/main/java/com/example/nikhiltrivedi/camera_component/CameraMain.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.camera2.CameraDevice ;
 import android.widget.Button;
+import android.hardware.Camera;
 
 
 public class CameraMain extends AppCompatActivity {
@@ -14,17 +15,30 @@ public class CameraMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_main);
-
-
-
     }
 
-    Button cam_button = new Button(findViewById("camera_button") ) ;
-
-    cam_button.setOnClickListener()
-
-
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+}
 
 
 
