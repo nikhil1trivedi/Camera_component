7af6c4213d24655d0aec95db4e31d913eddb4c2c
Nikhil Trivedi <ntrivedi@scu.edu>
2018-04-05 20:40:52 -0700

    Initial commit

---
+++ app/src/main/java/com/example/nikhiltrivedi/camera_component/CameraMain.java
@@ -0,0 +1,38 @@
+package com.example.nikhiltrivedi.camera_component;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.hardware.camera2.CameraDevice ;
+import android.widget.Button;
+
+
+public class CameraMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_main);
+
+
+
+    }
+
+    Button cam_button = new Button(findViewById("camera_button") ) ;
+
+    cam_button.setOnClickListener()
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/nikhiltrivedi/camera_component/camera_open.java
@@ -0,0 +1,85 @@
+package com.example.nikhiltrivedi.camera_component;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+import android.view.View;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraAccessException;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class camera_open extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_open);
+
+         //Preview Class
+            // must be passed to the amera before the live image preview is started
+
+
+
+        class Preview extends ViewGroup implements SurfaceHolder.Callback {
+            // the surface holder callback allows us to get notified of when the
+            // surface is created or destroyed
+            SurfaceView mSurfaceView;
+            SurfaceHolder mHolder ;
+
+            Preview(Context context) {
+                super(context);
+
+                mSurfaceView = new SurfaceView(context);
+                addView(mSurfaceView);
+
+
+
+                mHolder= mSurfaceView.getHolder();
+                mHolder.addCallback(this);
+                mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
