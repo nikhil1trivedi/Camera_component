b9f2fcd04aa99c861746b7946cdb0d22f4386c52
Nikhil Triveikhil Trivedi <ntrivedi@scu.edu>
2018-04-23 15:32:53 -0700

    Cam Commit

--- app/src/main/java/com/example/nikhiltrivedi/camera_component/camera_open.java
+++ app/src/main/java/com/example/nikhiltrivedi/camera_component/camera_open.java
@@ -22,7 +22,7 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraDevice.StateCallback;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.TotalCaptureResult;
@@ -122,64 +122,95 @@ TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceText
             }
 }:
 
-
+// the state callback will recieve  updates about the state of the camera
 private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback(){
     @Override
         public void onOpened(CameraDevice camera){
 
+        Log.e(TAG, "onOpened")
+        cameraDevice = camera ;
+        createCameraPreview() ;
+
+        }
+    @Override
+        public void onDisconnected(CameraDevice camera){
+            cameraDevice.close() ;
+
+
         }
 
-        public void onDisconnected(){
-
-        }
-
-        public void onError(){
-
+        public void onError(CameraDevice camera, int error){
+            cameraDevice.close() ;
+            cameraDevice = null ;
         }
     }
 
 
-    final CameraCaptureSession.CaptureCallback(){
-        public void onCaptureCompleted(){
+    final CameraCaptureSession.CaptureCallback captureCallbackListener = new CameraCaptureSession.CaptureCallback(){
+        @Override
+        public void onCaptureCompleted(CameraCaptureSession session,CaptureRequest request,TotalCaptureResult result){
+                super.onCamptureCompleted(session,request,result);
+                // Toast - provides simple feedback about an operature
+                    // in a small popup
+                    //automatically disappear after a timeout
+                Toast.makeText(camera_open.this,"Saved:"+file,Toast.LENGTH_SHORT).show();
+                createCameraPreview();
+
+                }
+        // add a on capture failure just in case it fails
+
+        @Override
+        public void onCaptureFailure(CameraCaptureSession session, CaptureRequest request, CaptureFailure failure){
+        console.log('ON CAPTURE FAILURE');
+        }
+
+    };
+
+        // Background Threads are used to run code in a seperate thread to prevent overflow and loss of speed
+        protected void startBackgroundThread(){
+            mBackgroundThread = new HandlerThread("Camera Background");
+            mBackgroundThread.start();
+            mBackgroundHandler = new Handler (mBackgroundThread.getLooper());
+
 
         }
 
-    }
-
-
-    protected void startBackgroundThread(){
-
-    }
-
-    protected void stopBackgroundThread(){
-
-    }
-
-    protected void takePicture(){
-
-    }
-
-
-    //Camera Characteristics
-    try {
-
-        }catch(){
-
-        }
-    // Image Reader listener
-    ImageReader.OnImageAvailable
-
-
-
-
-        private void save () {
+        protected void stopBackgroundThread(){
+            mBackgroundThread.quitSafely();
+            try{
+                mBackgroundThread.join();
+                mBackgroundThread = null;
+                mBackgroundHandler = null ;
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
 
         }
 
+        protected void takePicture(){
+            if(cameraDevice==null){
+            console.log("CAMERA DEVICE IS NULL")
+            return;
+            }
 
-    final CameraCaptureSession.CaptureCallback capturelistener =
+
+            //Camera Characteristics
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try{
+
+            }catch(CameraAccessException e){
+                e.printStackTrace(); 
+
+            }
+            // Image Reader listener
+            ImageReader.OnImageAvailable
 
 
+            private void save(){
+
+            }
+
+        }
 
      private void createCameraPreview(){
             try{
